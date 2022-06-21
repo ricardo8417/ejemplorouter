c6bc62477319eca4c6ab633131e012f5ad763c05
Ricardo <rnavarrete_cervantes@hotmail.com>
2022-06-20 22:42:53 -0500

    primer ejemplo de router

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+   return (
+      <div>
+         <Link to="/">
+            Cabeza
+         </Link>
+         -
+         <Link to="/cuerpo">
+            Cuerpo
+         </Link>
+         -
+         <Link to="/juntos">
+      Juntos
+   </Link>
+      </div>
+     
+   );
+};
+
+export default Header;
